3e4e37b1e8a2f315ecfa41b98b39e0a883bcfc44
AnastasiaDonets <anastasia.cheban@outlook.com>
2023-05-27 03:20:04 +0300

    add hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ImageGallery } from '../components/ImageGallery/ImageGallery';
 import { Modal } from '../components/Modal/Modal';
 import { Searchbar } from '../components/Searchbar/Searchbar';
@@ -7,87 +7,79 @@ import { Notify } from 'notiflix';
 import { Button } from '../components/Button/Button';
 import { Loader } from '../components/Loader/Loader';
 
-export class App extends Component {
-  state = {
-    images: [],
-    search: '',
-    page: 1,
-    isLoading: false,
-    openModal: false,
-    loadmoreBtn: false,
+export const App = () => {
+  const [images, setImages] = useState([]);
+  const [search, setSearch] = useState('');
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
+  const [largeImageURL, setLargeImageURL] = useState('');
+  const [loadMoreBtn, setLoadMoreBtn] = useState(false);
+
+  const onClickLoadMore = () => {
+    setPage(prev => prev + 1);
+    setIsLoading(true);
+    setLoadMoreBtn(true);
   };
 
-  onClickLoadMore = () => {
-    this.setState({
-      page: this.state.page + 1,
-      isLoading: true,
-    });
-    this.fetchGallery(this.state.search, this.state.page + 1);
+  const onOpenModal = url => {
+    setOpenModal(true);
+    setLargeImageURL(url);
   };
 
-  openModal = url => {
-    this.setState({ openModal: true, largeImageURL: url });
+  const onModalClose = () => {
+    setOpenModal(false);
+    setLargeImageURL('');
   };
 
-  onModalClose = () => {
-    this.setState({ openModal: false, largeImageURL: '' });
-  };
-
-  onSubmit = evt => {
+  const onSubmit = evt => {
     evt.preventDefault();
-    this.setState({
-      search: evt.target.search.value.trim().toLowerCase(),
-      isLoading: true,
-      images: [],
-      page: 1,
-    });
-    this.fetchGallery(evt.target.search.value);
+    setSearch(evt.target.search.value.trim().toLowerCase());
+    setIsLoading(true);
+    setImages([]);
+    setPage(1);
     evt.target.reset();
   };
 
-  async fetchGallery(search, page) {
-    try {
-      const response = await fetchImages(search, page);
-      this.setState(prevState => {
-        return {
-          images: [...prevState.images, ...response],
-        };
-      });
-      if (response.length < 1) {
-        Notify.failure('Not found');
-      }
-      if (response.length < 12) {
-        this.setState({ loadmoreBtn: false });
-      }
-      if (response.length === 12) {
-        this.setState({ loadmoreBtn: true });
-      }
-    } finally {
-      this.setState({ isLoading: false });
-    }
-  }
+  useEffect(() => {
+    if (!search) return;
+    const fetchGallery = async seach => {
+      try {
+        const response = await fetchImages(search, page);
+        setImages(prev => [...prev, ...response]);
+        if (response.length < 1) {
+          Notify.failure('Not found');
+        }
 
-  render() {
-    const { images, largeImageURL, isLoading, loadmoreBtn, openModal } =
-      this.state;
+        if (response.length < 12) {
+          setIsLoading(false);
+          setLoadMoreBtn(false);
+        }
 
-    return (
-      <div>
-        <Searchbar onSubmit={this.onSubmit} />
-        <ImageGallery
-          images={images}
-          openModal={this.openModal}
-          onLoadMore={this.onLoadMore}
-        />{' '}
-        {isLoading && <Loader />}
-        {loadmoreBtn && <Button onClickLoadMore={this.onClickLoadMore} />}
-        {openModal && (
-          <Modal
-            largeImageURL={largeImageURL}
-            onModalClose={this.onModalClose}
-          />
-        )}
-      </div>
-    );
-  }
-}
+        if (response.length === 12) {
+          setIsLoading(true);
+          setLoadMoreBtn(true);
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchGallery(search, page);
+  }, [search, page]);
+
+  return (
+    <div>
+      <Searchbar onSubmit={onSubmit} />
+      <ImageGallery
+        images={images}
+        openModal={onOpenModal}
+        onLoadMore={onClickLoadMore}
+      />{' '}
+      {isLoading && <Loader />}
+      {loadMoreBtn && <Button onClickLoadMore={onClickLoadMore} />}
+      {openModal && (
+        <Modal largeImageURL={largeImageURL} onModalClose={onModalClose} />
+      )}
+    </div>
+  );
+};
